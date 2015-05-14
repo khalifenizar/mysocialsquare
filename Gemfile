002b43b9ddd11a1b050733a1e82be646109bafb5
christopher pelnar <christopherpelnar@gmail.com>
2015-05-14 05:45:35 -0400

    enabled video content for animated gifs/ added controls/ formatted style to fit design

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 
 gem 'figaro'
+
 gem 'twitter_oauth'
 
 gem 'twitter'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,15 @@ class PostsController < ApplicationController
       config.access_token = current_user.twitter_token
       config.access_token_secret = current_user.twitter_token_secret
     end
-    @posts = client.user_timeline(count:200)
+    ungrouped_posts = client.user_timeline(count:200)
+    @posts = {}
+
+    ungrouped_posts.each do |post|
+     day = post.created_at.strftime('%m-%d-%Y')
+       if @posts[day].nil?
+        @posts[day] = {day: day, posts: [] }
+       end
+       @posts[day][:posts].push(post)
+    end
   end
 end
